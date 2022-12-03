69ad2094a2427597d0405607a4afe58933c0fd8e
Валерия Михайлова <valeriya23.04@outlook.com>
2022-12-03 16:20:56 +0300

    Добавьте файлы проекта.

---
+++ 5lecture13/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace lec13_ex5
+{
+    class Program
+    {
+        private static void CountingSort(int[] arr)
+        {
+            int max = arr.Max();
+            int min = arr.Min();
+
+            int[] count = new int[max - min + 1];
+            int z = 0;
+
+            for (int i = 0; i < count.Length; i++)
+            {
+                count[i] = 0;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                count[arr[i] - min]++;
+            }
+
+            for (int i = min; i <= max; i++)
+            {
+                while (count[i - min]-- > 0)
+                {
+                    arr[z] = i;
+                    z++;
+                }
+            }
+
+            foreach (var x in arr)
+            {
+                Console.Write(x + " ");
+            }
+        }
+    }
+}
